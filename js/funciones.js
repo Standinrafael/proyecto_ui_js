a23fcfb2fbb8329784eadcef268b0e20df45bd2d
Jorge Sanchez <jlsanchez1997@hotmail.es>
2022-12-07 19:45:41 -0500

    Taller 9

---
+++ js/funciones.js
@@ -0,0 +1,16 @@
+function dividir (num1,num2){
+    return num1/num2;
+}
+
+function multiplicar (num1,num2){
+    return num1*num2;
+}
+
+function sumar1 (num1,num2){
+    return num1+num2;
+}
+
+function restar (num1,num2){
+    return num1-num2;
+}
+
